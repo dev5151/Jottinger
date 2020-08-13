08f6a48ab0cb3ffe448686660fac912b7e8ea503
dev5151 <iamdev5151@gmail.com>
2020-08-13 22:43:05 +0530

    Added UI Improvements

--- app/src/main/java/com/dev5151/notezz/MainActivity.java
+++ app/src/main/java/com/dev5151/notezz/MainActivity.java
@@ -2,13 +2,31 @@ package com.dev5151.notezz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton fabAddNotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+
+        fabAddNotes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,NoteActivity.class));
+            }
+        });
+    }
+
+    private void initView(){
+        fabAddNotes=findViewById(R.id.fab);
     }
 }
